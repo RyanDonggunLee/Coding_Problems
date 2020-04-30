15782954ee5db8680f4bdb52efe2b820d5c19fcc
donggunLee-DG <donggun.lee.01@gmail.com>
2020-04-30 15:20:41 -0400

    test

--- src/BFSDFS/RotttingOrange.java
+++ src/BFSDFS/RotttingOrange.java
@@ -2,16 +2,9 @@ package BFSDFS;
 
 public class RotttingOrange {
     public int orangesRotting(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        if (row > 10 || col > 10)
-            return
-
-        int[] rotten = new int[row*col];
-
-        for(int rotten)
+        return 0;
     }
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
     }
 }
